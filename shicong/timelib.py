958b4515e5d1bff49b7013f4930571d3092227c2
shi-cong <shi_cong@icloud.com>
2017-10-06 07:49:25 +0800

    重构一下

--- shicong/timelib.py
+++ shicong/timelib.py
@@ -5,6 +5,11 @@ import time
 
 
 def func_runtime(callback):
+    """
+    测试函数执行的时间
+    :param callback: 函数
+    :return: 时间
+    """
     now = time.time()
     print('%s - start: %f' % (callback, now))
     callback()
@@ -13,19 +18,28 @@ def func_runtime(callback):
 
 
 def timestamp_to_time(timestamp):
+    """
+    时间戳转时间
+    :param timestamp: 时间戳
+    :return: 时间
+    """
     x = time.localtime(timestamp)
     return time.strftime('%Y-%m-%d %H:%M:%S', x)
 
 
 def time_to_timestamp(time_str):
+    """
+    时间字符串转时间戳
+    :param time_str: 时间字符串
+    :return: 时间戳
+    """
     x = time.strptime(time_str, '%Y-%m-%d %H:%M:%S')
     return time.mktime(x)
 
 
 def get_current_timestamp():
+    """
+    获取当前的时间戳
+    :return: 时间戳
+    """
     return time.time()
-
-
-if __name__ == '__main__':
-    ttt = timestamp_to_time(1507158811)
-    print(ttt)
