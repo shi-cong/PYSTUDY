adb2f0e5a76972a6fca8199733de6edcd74d6ad5
shi-cong <shi_cong@icloud.com>
2017-10-06 07:55:20 +0800

    重构一下

--- shicong/__init__.py
+++ shicong/__init__.py
@@ -1,3 +1,7 @@
+"""
+看书，学习，慢慢积累，不作重复的事情
+"""
+
 __all__ = ['ddoslib', 'encryptlib', 'excellib', 'html_parserlib', 'jsonlib', 'mongolib', 'mysqllib', 'numpylib',
            'oslib', 'pdflib', 'pillib', 'rabbitmqlib', 'randomlib', 'requestslib', 'tensorflowlib', 'timelib',
            'tornadolib', 'yamllib']
