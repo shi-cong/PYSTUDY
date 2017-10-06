4cdaf6340773090b374e94aa62e3554c10950473
shi-cong <shi_cong@icloud.com>
2017-10-06 22:06:08 +0800

    pypi

--- examples/mysqllib_demos/job51_db.py
+++ examples/mysqllib_demos/job51_db.py
@@ -1,13 +1,15 @@
 from shicong.mysqllib import MYSQLPool
+from shicong.yamllib import Yaml
 
 mysql_pool = MYSQLPool(20,
-                    **dict(host='192.168.80.4',
-                           user='job51',
-                           password='123456',
-                           db='job51',
-                           charset='utf8mb4'))
+                    **Yaml('db.yaml').get('job51'))
 
 def add_my_apply(*params):
+    """
+    增加我的投递
+    :param params:
+    :return:
+    """
     if len(params) != 7:
         raise Exception('参数不对')
     sql = 'insert into my_apply(job_name, pay, job_url, submit_time, submit_nums, company_name, company_url) ' \
@@ -15,6 +17,11 @@ def add_my_apply(*params):
     mysql_pool.execute(sql, params)
 
 def add_job(*params):
+    """
+    增加我的投递的工作
+    :param params:
+    :return:
+    """
     sql = 'insert into job(job_id, job_name, company, addr, money, pub_time) ' \
           'values(%s, %s, %s, %s, %s, %s)'
     mysql_pool.execute(sql, params)
--- examples/mysqllib_demos/ss_db.py
+++ examples/mysqllib_demos/ss_db.py
@@ -1,11 +1,8 @@
 from shicong.mysqllib import MYSQLPool
+from shicong.yamllib import Yaml
 
 mysql_pool = MYSQLPool(20,
-                    **dict(host='192.168.80.4',
-                           user='ss',
-                           password='123456',
-                           db='ss',
-                           charset='utf8mb4'))
+                    **Yaml('db.yaml').get('ss'))
 
 def get(keyword, page=1):
     keyword = '%keyword%'
