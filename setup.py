b3a10984ec8d9dddcb2dd454d118fa9c278d1704
shi-cong <shi_cong@icloud.com>
2017-10-06 20:58:51 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+if sys.argv[-1] == 'speedups':
+    try:
+        __import__('pip')
+    except ImportError:
+        print('Pip required.')
+        sys.exit(1)
+
+    os.system('pip install ujson')
+    sys.exit()
+
+if sys.argv[-1] == 'test':
+    try:
+        __import__('py')
+    except ImportError:
+        print('py.test required.')
+        sys.exit(1)
+
+    errors = os.system('py.test test_shicong.py')
+    sys.exit(bool(errors))
+
+packages = [
+    'shicong'
+]
+
+install = [
+    'bcrypt',
+    'scrapy',
+    'xlwt',
+    'pymongo',
+    'pymysql',
+    'numpy',
+    'pypdf2',
+    'reportlab',
+    'pytesseract',
+    'pillow',
+    'pika',
+    'requests',
+    'tensorflow',
+    'tornado',
+    'pyyaml',
+]
+
+
+with open('shicong/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name='shicong',
+    version=version,
+    description='A two package of code to facilitate future work efficiency and avoid repetitive open source projects.',
+    author='shi-cong',
+    author_email='shi_cong@icloud.com',
+    url='https://github.com/shi-cong/shicong',
+    packages=packages,
+    license='Apache License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    tests_require=['pytest'],
+    install_requires=install,
+)
--- shicong/__init__.py
+++ shicong/__init__.py
@@ -2,6 +2,14 @@
 看书，学习，慢慢积累，不作重复的事情
 """
 
+__title__ = 'shicong'
+__version__ = '0.12.1'
+__build__ = 0x001201
+__author__ = 'shi-cong'
+__license__ = 'Apache License'
+__copyright__ = 'Copyright 2017 shi-cong'
+__docformat__ = 'html'
+
 __all__ = ['ddoslib', 'encryptlib', 'excellib', 'html_parserlib', 'jsonlib', 'mongolib', 'mysqllib', 'numpylib',
            'oslib', 'pdflib', 'pillib', 'rabbitmqlib', 'randomlib', 'requestslib', 'tensorflowlib', 'timelib',
            'tornadolib', 'yamllib']
