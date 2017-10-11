cec574093a7ed61eeda3cbcb4537f421e22583de
shi-cong <shi_cong@icloud.com>
2017-10-12 01:42:10 +0800

    数据分析成交价格加权平均数（vwap）时间价格加权平均数（twap) 算数平均数(mean) 最大值和最小值计算亏损，中位数，用于去除预测某一个东西的趋势的干扰，方差（变量变化的程度，变动过于剧烈的股票会给投资者带来麻烦）

--- test/numpylib_test.py
+++ test/numpylib_test.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from shicong.numpylib import tile, arange, array, array_len
+from shicong.numpylib import tile, arange, array, array_len, eye, savetxt, loadtxt, vwap, mean, twap, weight, median
+import shicong.numpylib as nplb
 
 class NumpylibTestCase(TestCase):
     def test_1(self):
@@ -31,3 +32,24 @@ class NumpylibTestCase(TestCase):
         print(m)
         print(m[0, 0])
         print(m[0, 1])
+
+    def test_3_1(self):
+        i2 = eye(2, dtype=nplb.int)
+        print(i2)
+        savetxt("numpylib_data/eye.txt", i2)
+
+    def test_3_2(self):
+        """计算加权平均数"""
+        c, v = loadtxt('numpylib_data/abcdefg.csv', dtype=nplb.float, delimiter=',', usecols=(5,6), unpack=True)
+        vp =  vwap(c, weights=v)
+        print(c, v, vp)
+
+        print(mean(c))
+
+        print(twap(c, weights=weight(c)))
+
+        print(nplb.max(c), nplb.min(c))
+
+        print(nplb.median(c))
+
+        print(nplb.var(c))
