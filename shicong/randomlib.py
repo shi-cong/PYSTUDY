f2e04415178584002acdf77420564d08fa2b8e21
shi-cong <shi_cong@icloud.com>
2017-10-13 07:11:14 +0800

    修改了版本号

--- shicong/randomlib.py
+++ shicong/randomlib.py
@@ -53,6 +53,10 @@ def random_float(left=0, right=1):
     return random.uniform(left, right)
 
 def random_user_agent():
+    """
+    随机一个user-agent
+    :return:
+    """
     ua_list = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
         "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
