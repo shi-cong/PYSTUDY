00ecfe9a4b0228a8b7f97f673e5ce2b82f66dd23
shi-cong <shi_cong@icloud.com>
2017-10-13 06:48:48 +0800

    文档生成

--- shicong/stringlib.py
+++ shicong/stringlib.py
@@ -3,7 +3,48 @@ stirng模块
 """
 
 def endswith(text, end_str):
+    """
+    判断字符串是否以某一个字符串结尾
+    :param text: 数据
+    :param end_str: 关键字
+    :return: True or False
+    """
     return text.endswith(end_str)
 
 def join(strs, join_str):
+    """
+    拼接字符串
+    :param strs:  字符换列表
+    :param join_str: 拼接字符串名称
+    :return: 拼接的字符串
+    """
     return join_str.join(strs)
+
+def count(text, keyword):
+    """
+    获取keyword在text中的个数
+    :param text: 文章
+    :param keyword: 字符串
+    :return: 个数
+    """
+    return text.count(keyword)
+
+def split(text, keyword):
+    """
+    以制定关键字分割text
+    :param text: 数据
+    :param keyword: 关键字
+    :return: 分割的字符串列表
+    """
+    return text.split(keyword)
+
+
+def rsplit(text, keyword, c):
+    """
+    从右边开始分割字符串
+    :param text: 要分割的字符串
+    :param keyword: 关键字
+    :param c: 分割次数
+    :return: 分割的数组
+    """
+    return text.rsplit(keyword, c)
--- tool/gen_api_doc.py
+++ tool/gen_api_doc.py
@@ -3,7 +3,7 @@
 """
 from shicong.oslib import system, walk, isfile, isdir, split_ext, curdir, getcwd, mv, join
 import shicong
-from shicong.stringlib import endswith, join as j2
+from shicong.stringlib import endswith, join as j2, rsplit, count
 
 p = shicong.__path__[0]
 
@@ -14,18 +14,33 @@ for parent,dirnames,filenames in walk(p):
     if '__py' in parent:
         continue
     prefix = 'shicong'
-    print(prefix)
-
-    count = 0
+    count1 = 0
     for f in filenames:
-        if count == 0:
-            system('pydoc3 -w %s' % prefix)
+        print(parent)
 
-        if endswith(f, '.py'):
-            tmp = j2([prefix, split_ext(f)[0]], '.')
-            print(tmp)
-            system('pydoc3 -w %s' % tmp)
-        system('mv *.html  ../doc/')
+        if count1 == 0:
+            # 如果是第一个文件
+            if endswith(parent, 'shicong'):
+                system('pydoc3 -w %s' % prefix)
+            else:
+                tmp = rsplit(parent, 'shicong/', 1)[1]
+                system('pydoc3 -w %s' % j2([prefix, tmp], '.'))
+        else:
+            if endswith(f, '.py'):
+                # 如果是python文件
+                tmp1 = None
+                if count(parent, 'shicong/') == 2:
+                    tmp1 = rsplit(parent, 'shicong/', 1)[1]
+                tmp = j2([prefix, split_ext(f)[0]], '.')
+                if tmp1:
+                    tmp = j2([prefix, tmp1, split_ext(f)[0]], '.')
 
-        count += 1
+                system('pydoc3 -w %s' % tmp)
+            else:
+                tmp = rsplit(parent, 'shicong/', 1)[1]
+                system('pydoc3 -w %s' % j2([prefix, tmp, split_ext(f)[0]], '.'))
+
+        # system('mv *.html  ../doc/')
+
+        count1 += 1
     count = 0
