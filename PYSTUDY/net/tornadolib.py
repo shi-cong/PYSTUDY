a1406a5804aedfd2036644c1beb3dcfd6a6db3ec
shi-cong <shi_cong@icloud.com>
2017-10-30 14:42:17 +0800

    fix tornado tempalte static

--- PYSTUDY/net/tornadolib.py
+++ PYSTUDY/net/tornadolib.py
@@ -35,13 +35,31 @@ class Application:
         if is_subclass(handler, BaseHandler):
             self.views.append((view_name, handler))
 
+    def add_template_path(self, path):
+        """
+        增加模版文件路径
+        :param path: 路径 
+        :return:
+        """
+        self.templatePath = path
+
+    def add_static_path(self, path):
+        """
+        增加静态文件路径
+        :param path: 路径 
+        :return:
+        """
+        self.staticPath= path
+
     def start(self, port):
         """
         启动服务器
         :param port: 端口号
         :return:
         """
-        self.application = tornado.web.Application(self.views)
+        self.application = tornado.web.Application(self.views, 
+                template_path=self.templatePath, 
+                static_path=self.staticPath)
         self.application.listen(port)
         tornado.ioloop.IOLoop.instance().start()
 
