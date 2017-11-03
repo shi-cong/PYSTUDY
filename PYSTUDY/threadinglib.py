8b42004f6589e4bb2ffeb46018bf2fb77268fc20
shi-cong <shi_cong@icloud.com>
2017-11-03 12:42:08 +0800

    fix thread bug

--- PYSTUDY/threadinglib.py
+++ PYSTUDY/threadinglib.py
@@ -1,6 +1,6 @@
 from threading import Thread
 
-def create_thread(daemon=True, callback, *callbackParams):
+def create_thread(daemon, callback, *callbackParams):
     """创建一个线程
     :param daemon: True表示线程随着主线程关闭而关闭，False表示主线程必须等待子线程结束
     :param callback: 线程的回调函数
