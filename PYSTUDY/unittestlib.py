c1773e5581058f4098186ffa78dcb3486bbe25cc
shi-cong <shi_cong@icloud.com>
2017-11-08 16:46:31 +0800

    执行指定测试类的指定测试方法

--- PYSTUDY/unittestlib.py
+++ PYSTUDY/unittestlib.py
@@ -1,7 +1,7 @@
 """
 单元测试封装
 """
-from unittest import TestCase, TextTestRunner, TestLoader, main
+from unittest import TestCase, TextTestRunner, TestLoader, TestSuite
 
 TestCase = TestCase # 测试单元类
 run_all = main # 运行所有的测试单元
@@ -12,3 +12,13 @@ def run_test_case(testCase):
     """
     unit = TestLoader().loadTestsFromTestCase(testCase)
     TextTestRunner(verbosity=2).run(unit) 
+
+
+def run_one_test(testCase, testMethod):
+    """执行指定模块的指定测试方法
+    :param testCase: 测试类
+    :param testMethod: 测试方法
+    """
+    unit = TestSuite()
+    unit.addTest(testCase(testMethod))
+    unit.run(unit)
