9b813d737e4e65cceaa289fe7c4068de79b29aef
shi-cong <shi_cong@icloud.com>
2017-11-11 00:58:22 +0800

    udp扫描

--- test/threadinglib_test.py
+++ test/threadinglib_test.py
@@ -5,31 +5,38 @@ from PYSTUDY.pprintlib import pfomart
 from PYSTUDY.loglib import Logger
 from PYSTUDY.timelib import sleep
 from PYSTUDY.net.fingerlib import get_host_finger
-import socket
+from PYSTUDY.debuglib import trace_info
 
 
 LOG = Logger('threadinglibtest')
 
-def conn(host, port):
+def conn(protocol, host, port):
     try:
-        fg = get_host_finger(host, port)
+        fg = get_host_finger(protocol, host, port)
         LOG.log(pfomart([host, port, fg]))
     except Exception as e:
-        print(e)
+        print(trace_info())
 
 
 class ThreadinglibTest(TestCase):
+    def setUp(self):
+        self.host = '180.101.192.249'
+
     def test_thread(self):
-        host = 'www.whsjyr.net'
-        i = 0
-        while i < 100:
+        # host = 'www.whsjyr.net'
+        i = 1
+        protocol = 'udp'
+        while i < 65536:
             if active_count() <= 100: 
                 # 这里将False改为True，线程都不会运行，不是说不运行，主线程退出，其它线程都死掉了
-                create_thread(False, conn, host, i).start()
+                create_thread(False, conn, protocol, self.host, i).start()
                 i += 1
             else:
                 sleep(2)
 
+    def test_udp(self):
+        conn('udp', self.host, 27019)
+
 
 if __name__ == '__main__':
-    run_one_test(ThreadinglibTest, 'test_thread')
+    run_one_test(ThreadinglibTest, 'test_udp')
